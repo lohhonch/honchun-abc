1bb67820a08ca285b8a5d77b8b19d6414f110747
Loh Hon Chun <hongjun@outlook.com>
2024-10-14 23:23:34 +0800

    file uploader

--- helper/constant.py
+++ helper/constant.py
@@ -4,6 +4,3 @@ TITLE_LOGIN = "Tender Conflict Analyser"
 TITLE_ANALYSE = "Analyse"
 TITLE_ABOUT_US = "About Us"
 TITLE_METHODOLOGY = "Methodology"
-
-# Input configuration
-MAX_NUMBER_OF_FILES = 2
--- pages/analyse.py
+++ pages/analyse.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from helper import constant
+from helper import constant, utility
 from navigation import make_sidebar
 
 st.set_page_config(
@@ -14,14 +14,15 @@ st.write(st.session_state)
 st.title(f':material/rebase: {constant.TITLE_ANALYSE}')
 st.write("Start by uploading a set of documents below and we will analyse them for conflicts.")
 
+max_files = int(utility.get_keyvalue('MAX_NUMBER_OF_FILES'))
 # Documents uploader using `st.file_uploader`.
 uploaded_files = st.file_uploader(
-  label=f"Upload up to ***{constant.MAX_NUMBER_OF_FILES}*** documents (.docx or .pdf)", type=("docx", "pdf", "txt"),
+  label=f"Upload up to ***{max_files}*** documents (.docx or .pdf)", type=("docx", "pdf", "txt"),
   accept_multiple_files=True,
   key="documents_uploaded"
 )
 
-if len(uploaded_files) > constant.MAX_NUMBER_OF_FILES:
+if len(uploaded_files) > max_files:
   st.warning(
-    f'Maximum number of documents reached. Only the first {constant.MAX_NUMBER_OF_FILES} will be processed.')
-  uploaded_files = uploaded_files[:constant.MAX_NUMBER_OF_FILES]
+    f'Maximum number of documents reached. Only the first {max_files} will be processed.')
+  uploaded_files = uploaded_files[:max_files]
